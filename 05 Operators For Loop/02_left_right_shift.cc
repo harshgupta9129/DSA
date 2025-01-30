62f0c49cc8bd58981912dbe3a0aa3b2feb3be8bd
Harsh Gupta <rockstar221405@gmail.com>
2025-01-30 23:31:41 +0530

    Lecture 5 Codes And Questions Added

---
+++ 05 Operators For Loop/02_left_right_shift.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<(3<<2)<<endl; //left sift most of cases multiply with 2
+    cout<<(55>>2)<<endl; //Right sift most of cases divide by 2
+
+    return 0;
+}
