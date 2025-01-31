dc170b7d19262c75068e5551032dd42e154ed7ef
Harsh Gupta <rockstar221405@gmail.com>
2025-01-31 19:22:25 +0530

    Square Root Codes Updated

--- 14 Binary Search Set 2/03_squareroot_using_binary_search.cc
+++ 14 Binary Search Set 2/03_squareroot_using_binary_search.cc
@@ -1,16 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int mysqrt (int num) {
-    int si = 0, ei = num;
-    while (si<ei) {
-        int mid = si + (ei - si)/2;
-        if (mid*mid==num) return mid;
-        if (mid*mid<num) si = mid + 1;
-        else ei = mid;
+int binarySearch(int n) {
+    if (n == 0 || n == 1) return n;  // Handle small cases
+
+    int s = 0, e = n, ans = 1;
+    while (s <= e) {
+        long long mid = s + (e - s) / 2;
+        long long squ = mid * mid;
+
+        if (squ == n) return mid;  // Exact square root found
+
+        if (squ < n) {  // Move right
+            ans = mid;
+            s = mid + 1;
+        } else {  // Move left
+            e = mid - 1;
+        }
     }
-    return -1;
+    return ans;  // Floor square root
 }
+int mySqrt(int x) {
+    return binarySearch(x);
+}
+
 int main() {
-    cout<<mysqrt(10000)<<endl;
+    cout<<mySqrt(5)<<endl;
     return 0;
 }
