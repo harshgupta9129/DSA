3ed3452f3d04cdb5d9cc78d1f13a0041f9ed870e
Harsh Gupta <rockstar221405@gmail.com>
2025-02-14 18:21:21 +0530

    Lecture 28 Codes And Question Added

---
+++ 028 Reference Variable/03_release_dynamic.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // while (true) {
+    //     int * ptr = new int;
+    // }
+    // do not run th eabove code it will slow yours laptop or computer
+    return 0;
+}
