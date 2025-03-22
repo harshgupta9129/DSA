5120cf3e2ce5ccff4356eb54700109376b05ac30
harshgupta9129 <rockstar221405@gmail.com>
2025-03-22 21:49:33 +0530

    Lecture 02 Source Code Added

---
+++ 002 First Program/02_data_types.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    cout<<a<<endl;
+    float b = 4.5;
+    cout<<b<<endl;
+    double c = 5.98;
+    cout<<c<<endl;
+    char d = 'G';
+    cout<<d<<endl;
+    bool e = true;
+    cout<<e<<endl;
+    cout<<sizeof(a)<<endl; //Size of Int
+    cout<<sizeof(c)<<endl; //Size of Double
+return 0;
+}
---
+++ 002 First Program/05_unsigned_int.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    unsigned int a = 122;
+    cout<<a<<endl;
+    unsigned int b = -122; //it convert into 2,s copliment but treated as positive integer So Gives LArge Number
+    cout<<b<<endl;
+    return 0;
+}
